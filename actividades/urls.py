f36f813f3249b6f8ff2528c1ec3d35010f01eaae
xangel1111 <alexandro.alarcon@tecsup.edu.pe>
2025-04-09 16:11:56 -0500

    crud

--- actividades/urls.py
+++ actividades/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.lista_actividades, name='lista_actividades'),
     path('agregar/', views.agregar_actividad, name='agregar_actividad'),
+    path('editar/<int:pk>/', views.editar_actividad, name='editar_actividad'),
+    path('eliminar/<int:pk>/', views.eliminar_actividad, name='eliminar_actividad'),
 ]
--- actividades/views.py
+++ actividades/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Actividad
 from .forms import ActividadForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def lista_actividades(request):
     actividades = Actividad.objects.all()
@@ -16,3 +18,18 @@ def agregar_actividad(request):
         form = ActividadForm()
     return render(request, 'actividades/agregar_actividad.html', {'form': form})
 
+def editar_actividad(request, pk):
+    actividad = Actividad.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ActividadForm(request.POST, instance=actividad)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_actividades')  # Redirigir a la lista de actividades
+    else:
+        form = ActividadForm(instance=actividad)
+    return render(request, 'actividades/editar_actividad.html', {'form': form})
+
+def eliminar_actividad(request, pk):
+    actividad = Actividad.objects.get(pk=pk)
+    actividad.delete()
+    return HttpResponseRedirect(reverse('lista_actividades'))
